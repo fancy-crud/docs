4845d5a0a6786c2bb3e354fd9ce3479234b73553
Christopher A. Flores <christopher.flores@codexitos.com>
2023-06-13 21:34:35 -0600

    fix: add base path for assets

--- src/.vitepress/config.ts
+++ src/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/docs',
   title: "FancyCRUD",
   description: "Create forms and data tables with ease",
   themeConfig: {
